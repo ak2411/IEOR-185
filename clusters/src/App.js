69db80dd19f36926fa01c9330a71c86ed468bb0b
fedekunze <fekunze@berkeley.edu>
2017-11-28 02:26:54 -0800

    email validation

--- clusters/src/App.js
+++ clusters/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navb from './Navb';
-import './App.css';
 import {Link} from 'react-router-dom';
 
 class App extends Component {
--- clusters/src/index.js
+++ clusters/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './Main';
 import App from './App';
+import SignUp from './SignUp';
 import Home from './Home';
 import About from './About';
 import {Route, BrowserRouter} from 'react-router-dom';
@@ -11,7 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <BrowserRouter>
     <div>
-      <Route exact path='/' component={App} />
+      <Route exact path='/' component={SignUp} />
       <Route exact path='/about' component={About} />
       <Route exact path='/home' component={Home} />
     </div>
